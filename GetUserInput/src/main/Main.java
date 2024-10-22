6b1abcf81d69bb4be824f611e20bf2dc519081e2
MykolaDekusar <ndekusar97@gmail.com>
2024-10-22 17:27:51 +0200

    First use of Scanner to get user info using OOP to create a new Scanner(System.in) and using scanner.nextLine

--- GetUserInput/src/main/Main.java
+++ GetUserInput/src/main/Main.java
@@ -1,10 +1,17 @@
 package main;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in); //programmazione ad oggetti
+		//Stampiamo cosa vogliamo chiedere all'utente
+		System.out.println("Come ti chiami?");
+		//assegnamo ad una variabile quello che l'utente scriverà
+		String nome = scanner.nextLine();
+		System.out.println("Il tuo nome e': "+nome);
 	}
 
 }
+ 
