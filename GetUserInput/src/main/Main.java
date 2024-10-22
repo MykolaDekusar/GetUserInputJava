484ce3de3781ff422724f7e6c4519ba3da255522
MykolaDekusar <ndekusar97@gmail.com>
2024-10-22 18:01:57 +0200

    Use of scanner.nextLine(), scanner.nextInt() and reset of scanner.nextInt() to get a new scanner.nextLine().

--- GetUserInput/src/main/Main.java
+++ GetUserInput/src/main/Main.java
@@ -8,9 +8,20 @@ public class Main {
 		Scanner scanner = new Scanner(System.in); //programmazione ad oggetti
 		//Stampiamo cosa vogliamo chiedere all'utente
 		System.out.println("Come ti chiami?");
+		
 		//assegnamo ad una variabile quello che l'utente scriverà
 		String nome = scanner.nextLine();
+		System.out.println("Quanti anni hai?");
+		int eta = scanner.nextInt();//legge solo numeri interi altrimenti crea un Exception
+		System.out.println("Che lavoro fai?");
+		//dopo lo scanner.nextInt() dobbiamo resettare il nextLine perche legge il \n cioe a capo
+		scanner.nextLine();
+		String lavoro= scanner.nextLine();
 		System.out.println("Il tuo nome e': "+nome);
+		System.out.println("Hai: "+eta+" anni");
+		System.out.println("Il tuo lavoro è: "+lavoro);
+		
+		
 		scanner.close();
 	}
 
